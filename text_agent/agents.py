ea188f7bd9bd73a61d28f0b35feb137fb70a0b72
BioBootloader <thebiobootloader@gmail.com>
2024-04-06 13:02:49 -0700

    agents

--- text_agent/agents.py
+++ text_agent/agents.py
@@ -1,4 +1,7 @@
+import random
 from abc import ABC, abstractmethod
+
+from termcolor import cprint
 
 
 class AgentInterface(ABC):
@@ -6,3 +9,35 @@ class AgentInterface(ABC):
     def choose_next_action(
         self, last_observation: str, valid_actions: list[str], immediate_reward: int, score: int
     ) -> str: ...
+
+
+class RandomAgent(AgentInterface):
+    def choose_next_action(
+        self, last_observation: str, valid_actions: list[str], immediate_reward: int, score: int
+    ) -> str:
+        return random.choice(valid_actions)
+
+
+class HumanAgent(AgentInterface):
+    def choose_next_action(
+        self, last_observation: str, valid_actions: list[str], immediate_reward: int, score: int
+    ) -> str:
+        cprint(f"Observation: {last_observation}", "green")
+        cprint(f"Reward: {immediate_reward}", "yellow")
+        cprint(f"Score: {score}", "magenta")
+        cprint(f"Valid Actions: {valid_actions}", "green")
+        return input("Enter your action: ")
+
+
+class RawHistoryAgent(AgentInterface):
+    def __init__(self):
+        self.history = []
+
+    def choose_next_action(
+        self, last_observation: str, valid_actions: list[str], immediate_reward: int, score: int
+    ) -> str:
+        self.history.append((last_observation, valid_actions, immediate_reward, score))
+        return random.choice(valid_actions)
+
+    def get_history(self):
+        return self.history
